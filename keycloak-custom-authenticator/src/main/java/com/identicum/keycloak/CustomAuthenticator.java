8f07341eb3941b2ea97ab2c5cf88dc1abb886218
AbigailJustiniano <70248439+debora2004@users.noreply.github.com>
2023-02-06 17:15:35 -0300

    ISS-2 config CustomAutheticator

--- keycloak-custom-authenticator/src/main/java/com/identicum/keycloak/CustomAuthenticator.java
+++ keycloak-custom-authenticator/src/main/java/com/identicum/keycloak/CustomAuthenticator.java
@@ -1,13 +1,18 @@
 package com.identicum.keycloak;
 import org.jboss.logging.Logger;
+import org.jboss.resteasy.specimpl.MultivaluedMapImpl;
 
 import java.util.Random;
 
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
 
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.authenticators.browser.UsernamePasswordForm;
 import org.keycloak.forms.login.LoginFormsProvider;
+import org.keycloak.protocol.oidc.OIDCLoginProtocol;
+import org.keycloak.services.managers.AuthenticationManager;
+
 import static org.jboss.logging.Logger.getLogger;
 
 
@@ -17,20 +22,46 @@ public class CustomAuthenticator extends UsernamePasswordForm {
 	@Override
 	public void authenticate(AuthenticationFlowContext context) {
 		// Generar codigo random y mostrarlo en el theme
+		MultivaluedMap<String, String> formData = new MultivaluedMapImpl<>();
+        String loginHint = context.getAuthenticationSession().getClientNote(OIDCLoginProtocol.LOGIN_HINT_PARAM);
+        String rememberMeUsername = AuthenticationManager.getRememberMeUsername(context.getRealm(), context.getHttpRequest().getHttpHeaders());
+		
 		Random token =  new Random();
-		logger.infov("TOKEN!!!: {0}", token);
-		LoginFormsProvider form = context.form();
+		logger.infov("TOKEN!!!: {0}", token); //Que aparezca en los logs de Keycloak container
+	
 
-
-		super.authenticate(context);
+        if (context.getUser() != null) {
+            LoginFormsProvider form = context.form();
+            form.setAttribute(LoginFormsProvider.USERNAME_HIDDEN, true);
+            form.setAttribute(LoginFormsProvider.REGISTRATION_DISABLED, true);
+            context.getAuthenticationSession().setAuthNote(USER_SET_BEFORE_USERNAME_PASSWORD_AUTH, "true");
+        } else {
+            context.getAuthenticationSession().removeAuthNote(USER_SET_BEFORE_USERNAME_PASSWORD_AUTH);
+            if (loginHint != null || rememberMeUsername != null) {
+                if (loginHint != null) {
+                    formData.add(AuthenticationManager.FORM_USERNAME, loginHint);
+                } else {
+                    formData.add(AuthenticationManager.FORM_USERNAME, rememberMeUsername);
+                    formData.add("rememberMe", "on");
+                }
+            }
+        }
+        Response challengeResponse = challenge(context, formData);
+        context.challenge(challengeResponse);
 	}
 
 	@Override
 	public void action(AuthenticationFlowContext context) {
 		// Obtener informacion de la request y validar si es igual al codigo generado
 		MultivaluedMap<String, String> formData = context.getHttpRequest().getDecodedFormParameters();
-		
-		super.action(context);
+		if (formData.containsKey("cancel")) {
+            context.cancelLogin();
+            return;
+        }
+        if (!validateForm(context, formData)) {
+            return;
+        }
+        context.success();
 	}
 
 }
