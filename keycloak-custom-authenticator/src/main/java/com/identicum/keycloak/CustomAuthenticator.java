420808defed3124f483627f9121198ad5db8e636
AbigailJustiniano <70248439+debora2004@users.noreply.github.com>
2023-02-06 16:54:53 -0300

    ISSUE 1 and Part of Issue 2

--- keycloak-custom-authenticator/src/main/java/com/identicum/keycloak/CustomAuthenticator.java
+++ keycloak-custom-authenticator/src/main/java/com/identicum/keycloak/CustomAuthenticator.java
@@ -1,17 +1,26 @@
 package com.identicum.keycloak;
+import org.jboss.logging.Logger;
+
+import java.util.Random;
 
 import javax.ws.rs.core.MultivaluedMap;
 
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.authenticators.browser.UsernamePasswordForm;
 import org.keycloak.forms.login.LoginFormsProvider;
+import static org.jboss.logging.Logger.getLogger;
+
 
 public class CustomAuthenticator extends UsernamePasswordForm {
+	private static final Logger logger = getLogger(CustomAuthenticator.class);
 
 	@Override
 	public void authenticate(AuthenticationFlowContext context) {
 		// Generar codigo random y mostrarlo en el theme
+		Random token =  new Random();
+		logger.infov("TOKEN!!!: {0}", token);
 		LoginFormsProvider form = context.form();
+
 
 		super.authenticate(context);
 	}
