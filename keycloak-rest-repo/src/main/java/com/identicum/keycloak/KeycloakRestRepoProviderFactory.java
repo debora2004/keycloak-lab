9322bbedff8257078b3ad720ee6eb8165241bb97
AbigailJustiniano <70248439+debora2004@users.noreply.github.com>
2023-02-09 10:31:36 -0300

    Fix Mmock

--- keycloak-rest-repo/src/main/java/com/identicum/keycloak/KeycloakRestRepoProviderFactory.java
+++ keycloak-rest-repo/src/main/java/com/identicum/keycloak/KeycloakRestRepoProviderFactory.java
@@ -7,6 +7,9 @@ import org.keycloak.models.KeycloakSession;
 import org.keycloak.provider.ProviderConfigProperty;
 import org.keycloak.provider.ProviderConfigurationBuilder;
 import org.keycloak.storage.UserStorageProviderFactory;
+import static com.identicum.keycloak.RestConfiguration.PROPERTY_BASE_URL;
+import static org.keycloak.provider.ProviderConfigProperty.STRING_TYPE;
+
 
 import java.util.List;
 import static org.jboss.logging.Logger.getLogger;
@@ -22,6 +25,11 @@ public class KeycloakRestRepoProviderFactory implements UserStorageProviderFacto
 		logger.infov("Initializing Keycloak Rest Repo factory version: " + getClass().getPackage().getImplementationVersion());
 
 		ProviderConfigurationBuilder builder = ProviderConfigurationBuilder.create();
+		builder.property().name(PROPERTY_BASE_URL)
+				.type(STRING_TYPE).label("Base URL")
+				.defaultValue("http://localhost:8083/")
+				.helpText("Api url base to authenticate users")
+				.add();
 		configMetadata = builder.build();
 	}
 
