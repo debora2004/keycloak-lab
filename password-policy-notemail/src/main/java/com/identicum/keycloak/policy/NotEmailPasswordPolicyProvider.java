efb038a02cb42272305fc76cf211cdb5a9730224
AbigailJustiniano <70248439+debora2004@users.noreply.github.com>
2023-02-27 17:54:01 -0300

    ISSUE CASE POLICY

---
+++ password-policy-notemail/src/main/java/com/identicum/keycloak/policy/NotEmailPasswordPolicyProvider.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ package com.identicum.keycloak.policy;
+ import org.jboss.logging.Logger;
+ import static org.jboss.logging.Logger.getLogger;
+
+import org.keycloak.models.KeycloakContext;
+ import org.keycloak.models.RealmModel;
+ import org.keycloak.models.UserModel;
+import org.keycloak.policy.PasswordPolicyProvider;
+import org.keycloak.policy.PolicyError;
+ 
+ /**
+  * A {@link PasswordPolicyProvider} which does not allow to use the current email as password.
+  *
+  * @author <a href="mailto:thomas.darimont@googlemail.com">Thomas Darimont</a>
+  */
+ public class NotEmailPasswordPolicyProvider implements PasswordPolicyProvider {
+ 
+     private static final String ERROR_MESSAGE = "invalidPasswordNotEmailMessage";
+     private static final PolicyError POLICY_ERROR = new PolicyError(ERROR_MESSAGE);
+ 	 private static final Logger logger = getLogger(NotEmailPasswordPolicyProvider.class);
+
+     private KeycloakContext context;
+ 
+     public NotEmailPasswordPolicyProvider(KeycloakContext context) {
+         this.context = context;
+     }
+ 
+     @Override
+     public PolicyError validate(String email, String password) {
+         if (email == null) {
+             return null;
+         }
+         logger.infov("email!!!: {0}", email); //Que aparezca en los logs de Keycloak container
+
+         return email.toLowerCase().equals(password) ? POLICY_ERROR : null;
+     }
+ 
+     @Override
+     public PolicyError validate(RealmModel realm, UserModel user, String password) {
+         return validate(user.getEmail(), password);
+     }
+ 
+     @Override
+     public Object parseConfig(String value) {
+         return null;
+     }
+ 
+     @Override
+     public void close() {
+         // NOOP
+     }
+ 
+ }
